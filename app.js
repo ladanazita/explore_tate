b0dbc7c3e210247ba41850f35975779cf66751c8
Ladan Nasserian <ladannasserian@gmail.com>
2015-06-02 10:19:38 -0700

    specified artist route, returns artist name

--- app.js
+++ app.js
@@ -2,20 +2,27 @@
 var express = require('express'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
-  methodOverride = require('method-override');
+  cookieParser = require('cookie-parser');
 
 var app = express()
   // set port
   var port = process.env.PORT || 3000;
 
   // connect to mongoDB
-  mongoose.connect('mongodb://localhost/tate');
+  mongoose.connect('mongodb://localhost/tate', function(err){
+    if(err){
+      console.log('Wuh Oh! Connectin erro', err);
+    } else{
+      console.log('Its magical here at ' + port);
+    }
+  });
 
   //get POST params
   app.use(bodyParser.json());
 
 
   var artworkSchema ={
+    id: Number,
     artist: String,
     title: String,
     thumbnailUrl: String
@@ -26,20 +33,17 @@ var Artwork = mongoose.model('Artwork', artworkSchema, 'artworks')
 
 
 // api route
-app.get('/api/artworks', function (req, res){
-  Artwork.findOne(function(err,art){
+app.get('/api/artworks/:id', function (req, res, next){
+  Artwork.findById(req.params.id,function(err,art){
     if (err)
       res.send(err);
-
-    res.json(art);
+    res.json(art.artist);
   });
 });
 
 // frontend route
 app.get('*', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('./index.html');
 });
 
 app.listen(port);
-
-console.log('Its magical here at ' + port);
