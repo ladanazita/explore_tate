50c0010d146f5f11bff33387dc640c431a767904
Ladan Nasserian <ladannasserian@gmail.com>
2015-06-03 12:54:33 -0700

    modified env heroku

--- public/js/visualArt.js
+++ public/js/visualArt.js
@@ -2,7 +2,7 @@ var d3 = require('d3');
 
 var art;
 
-d3.json('/routes/api/v1/artists', function(err, art){
+d3.json('/routes/api/v1/atworks/artists', function(err, art){
   if (err){
     console.log(err);
   }else{
