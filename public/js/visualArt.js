738e49dcce4cdb9b2d5c227168823fdbfdcdc37c
Ladan Nasserian <ladannasserian@gmail.com>
2015-06-04 10:32:31 -0700

    practicing d3

--- public/js/visualArt.js
+++ public/js/visualArt.js
@@ -5,11 +5,17 @@ var artists = d3.json('/api/v1/arts/artists', function(err, data) {
   else{
     console.log ('Success');
   }
-  d3.select('body').selectAll('p')
-    .data(artists)
-    .enter()
-    .append("p")
-    .text(data);
+  // selects <body>
+  d3.select('body')
+  // add an <svg> html element
+    .append('svg')
+      .attr('width', 250)
+      .attr('height', 50)
+    .append('text')
+      .data(artists)
+      .text(d.artists)
+      .attr('x', "150")
+      .attr('y', "20")
 });
 
 
